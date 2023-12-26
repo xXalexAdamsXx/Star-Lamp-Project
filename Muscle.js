06638da71a7e622c39de961c0d7393f6c688c149
xXalexAdamsXx <alexadams6@icloud.com>
2023-12-25 18:27:31 -0800

    Filled out the basic skeleton, style, and functionality of the website

--- Muscle.js
+++ Muscle.js
@@ -1,4 +1,3 @@
-
 //This object stores brother information
 var brotherObj = {
     username : "person1",
@@ -11,15 +10,16 @@ var brotherObj = {
 function getUserInfo(){
     let username = Star_Lamp.getElementById('username').value
     let password = Star_Lamp.getElementById('password').value
-    brotherExists(username, password);
+    alert("You entered: " + username + " " + password )
+    brotherExists(username, password)
 }
+
 /**
  * Performs a linear search through the brotherhood database to see if the user exists
  * @param {String} name Username of the user
  * @param {String} pass Password of the user
  */
-
-function brotherExists(name, pass){
+function brotherExists(username, password){
     for(let i = 0; i < brotherObj.length; i++){
         if(username == brotherObj[i].username && password == brotherObj[i].password){
             alert("You are now signed in!")
