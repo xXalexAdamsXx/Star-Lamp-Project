15b603b677d5c0074782641d8c1388877966e071
xXalexAdamsXx <alexadams6@icloud.com>
2023-12-25 18:39:34 -0800

    Basic functionality actually works now :)

--- Muscle.js
+++ Muscle.js
@@ -1,16 +1,17 @@
 //This object stores brother information
-var brotherObj = {
-    username : "person1",
-    password : "pass"
-}
+var brotherObj = [
+    {
+        username : "person1",
+        password : "pass"
+    }
+]
 
 /**
  * Retreives user information from the login page
  */
 function getUserInfo(){
-    let username = Star_Lamp.getElementById('username').value
-    let password = Star_Lamp.getElementById('password').value
-    alert("You entered: " + username + " " + password )
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
     brotherExists(username, password)
 }
 
@@ -20,7 +21,9 @@ function getUserInfo(){
  * @param {String} pass Password of the user
  */
 function brotherExists(username, password){
+    console.log(brotherObj[0].username)
     for(let i = 0; i < brotherObj.length; i++){
+        //console.log("Username: " + brotherObj[i].username + "\n Password: " + brotherObj[i].password)
         if(username == brotherObj[i].username && password == brotherObj[i].password){
             alert("You are now signed in!")
             window.location.href = "http://www.themostamazingwebsiteontheinternet.com"
